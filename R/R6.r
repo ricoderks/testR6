2814458053c151ed81d1e45d7736b0435fdb9b1b
Rico Derks <r.j.e.derks@lumc.nl>
2024-10-01 08:37:25 +0200

    cleaning

--- R/R6.r
+++ R/R6.r
@@ -1,7 +1,15 @@
+#' @title R6 classes
+#'
+#' @description
+#' Several R6 classes for testing. One main class which will have 2 nested
+#' classes.
+#'
 #' @importFrom R6 R6Class
+#' @importFrom shiny reactive reactiveVal
 #'
 testR6 <- R6::R6Class(
   classname = "testR6",
+#------------------------------------------------------------------ private ----
   private = list(
     name = "",
     data = data.frame(x = stats::rnorm(n = 1000)),
@@ -14,6 +22,7 @@ testR6 <- R6::R6Class(
     },
     count = 0
   ),
+#------------------------------------------------------------------- public ----
   public = list(
     n1 = NULL,
     n2 = NULL,
@@ -28,15 +37,15 @@ testR6 <- R6::R6Class(
     reactive = function() {
       # Ensure the reactive stuff is initialized.
       if (is.null(private$reactiveExpr)) {
-        private$reactiveDep <- reactiveVal(0)
-        private$reactiveExpr <- reactive({
+        private$reactiveDep <- shiny::reactiveVal(0)
+        private$reactiveExpr <- shiny::reactive({
           private$reactiveDep()
           self
         })
       }
       private$reactiveExpr
     },
-    #----------------------------------------------------------------- data ----
+#--------------------------------------------------------------------- data ----
     changeData = function(newData) {
       private$data <- newData
       private$invalidate()
@@ -46,9 +55,12 @@ testR6 <- R6::R6Class(
   )
 )
 
-set.seed(123)
+
+#-------------------------------------------------------------- sub classes ----
+#------------------------------------------------------------------ class 1 ----
 testR61 <- R6::R6Class(
   classname = "testR61",
+#------------------------------------------------------------------ private ----
   private = list(
     name = "",
     n1 = NULL,
@@ -62,6 +74,7 @@ testR61 <- R6::R6Class(
     },
     count = 0
   ),
+#------------------------------------------------------------------- public ----
   public = list(
     initialize = function(name) {
       private$reactiveDep <- function(x) NULL
@@ -70,15 +83,15 @@ testR61 <- R6::R6Class(
     reactive = function() {
       # Ensure the reactive stuff is initialized.
       if (is.null(private$reactiveExpr)) {
-        private$reactiveDep <- reactiveVal(0)
-        private$reactiveExpr <- reactive({
+        private$reactiveDep <- shiny::reactiveVal(0)
+        private$reactiveExpr <- shiny::reactive({
           private$reactiveDep()
           self
         })
       }
       private$reactiveExpr
     },
-    #-------------------------------------------------------------------- n ----
+#------------------------------------------------------------------------ n ----
     changeN1 = function(newN) {
       private$n1 <- newN
       private$invalidate()
@@ -89,9 +102,11 @@ testR61 <- R6::R6Class(
   )
 )
 
-set.seed(123)
+
+#------------------------------------------------------------------ class 2 ----
 testR62 <- R6::R6Class(
   classname = "testR62",
+#------------------------------------------------------------------ private ----
   private = list(
     name = "",
     n2 = NULL,
@@ -104,6 +119,7 @@ testR62 <- R6::R6Class(
     },
     count = 0
   ),
+#------------------------------------------------------------------- public ----
   public = list(
     initialize = function(name) {
       private$reactiveDep <- function(x) NULL
@@ -112,15 +128,15 @@ testR62 <- R6::R6Class(
     reactive = function() {
       # Ensure the reactive stuff is initialized.
       if (is.null(private$reactiveExpr)) {
-        private$reactiveDep <- reactiveVal(0)
-        private$reactiveExpr <- reactive({
+        private$reactiveDep <- shiny::reactiveVal(0)
+        private$reactiveExpr <- shiny::reactive({
           private$reactiveDep()
           self
         })
       }
       private$reactiveExpr
     },
-    #-------------------------------------------------------------------- n ----
+#------------------------------------------------------------------------ n ----
     changeN2 = function(newN) {
       private$n2 <- newN
       private$invalidate()
